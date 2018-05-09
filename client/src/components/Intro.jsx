4052592fa8d8165fe834700b2e823ee71c2794e5
Michael Wachell <michaelwachell@Michaels-Mac-Pro.local>
2018-05-09 16:20:04 -0700

    make mvp

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -7,6 +7,7 @@ import Images from './Images.js'
 import './App.scss'
 import Dep from '../../../Assets/coding_things.png'
 import Me from '../../../Assets/me2.png'
+import Media from '../../../Assets/media_things.png'
 
 const Page = ({
   offset,
@@ -26,6 +27,13 @@ const Page = ({
   <Parallax.Layer offset={offset} speed={-0.2} onClick={onClick}>
     <div className={`slopeEnd ${gradient}`}/>
   </Parallax.Layer>
+  <Parallax.Layer className="image-container" offset={offset} speed={0.8} onClick={onClick}>
+  {
+  images && offset===0 ?  (<div className="portrait"><img width="30%" src={images}/></div>) :  null
+  }
+
+
+  </Parallax.Layer>
 
   <Parallax.Layer className="text header" offset={offset} speed={0.4}>
 
@@ -33,6 +41,7 @@ const Page = ({
       <p style={{
           fontSize: "calc(8vw + 10%)"
         }}>{caption}</p>
+
       <p style={{
           fontSize: "calc(3vw + 10%)"
         }}>{first} </p>
@@ -49,7 +58,7 @@ const Page = ({
 
   <Parallax.Layer className="image-container" offset={offset} speed={0.8} onClick={onClick}>
   {
-  images ?  (<div className="images"><img width="100%" src={images}/></div>) : null
+  images && offset!==0 ? (<div className="images"><img width="80%" src={images}/></div>) :  null
   }
 
 
@@ -75,7 +84,7 @@ export default class Intro extends Component {
       <Page offset={0} gradient="teal" className="roundMe" images={Me} caption="Hi!" first="My name is Michael..." second="*click or tap*" onClick={() => this.scroll(1)}/>
       <Page offset={1} gradient="greyf" caption="I am a media polymath" first="" second="" onClick={() => this.scroll(2)}/>
       <Page offset={2} gradient="black" images={Dep} caption="I create..." first="the web using these tools" second="" onClick={() => this.scroll(3)}/>
-      <Page offset={3} gradient="teal"  caption="I create..." first="media using these tools" second="" onClick={() => this.scroll(0)}/>
+      <Page offset={3} gradient="teal" images={Media} caption="I create..." first="media using these tools" second="" onClick={() => this.scroll(0)}/>
     </Parallax>)
   }
 }

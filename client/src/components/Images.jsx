2b00e55951705c1858333a416810f5194e4dd17b
Michael Wachell <michaelwachell@Michaels-Mac-Pro.local>
2018-05-25 15:31:37 -0700

    fixed stupids

--- client/src/components/Images.jsx
+++ client/src/components/Images.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import tnes from '../../../Assets/Design/TNES_LOGO.png'
-import drip1 from '../../../Assets/Design/ATD_G.png'
-import drip2 from '../../../Assets/Design/ATD_L.png'
-import lit from '../../../Assets/Design/Literary.png'
-import box from '../../../Assets/Design/box_smack.gif'
-import data from '../../../Assets/Design/data_spring.gif'
-import responsive from '../../../Assets/Design/responsive.gif'
-import fire from '../../../Assets/Design/fire_opto.gif'
+import tnes from '../../../Assets/design/TNES_LOGO.png'
+import drip1 from '../../../Assets/design/ATD_G.png'
+import drip2 from '../../../Assets/design/ATD_L.png'
+import lit from '../../../Assets/design/Literary.png'
+import box from '../../../Assets/design/box_smack.gif'
+import data from '../../../Assets/design/data_spring.gif'
+import responsive from '../../../Assets/design/responsive.gif'
+import fire from '../../../Assets/design/fire_opto.gif'
 module.exports = {
 
     first: [responsive, box, lit, data, fire, tnes, drip2, drip1]

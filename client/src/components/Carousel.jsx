e85a76602a7b8252520724827bf21c14e5989beb
Michael Wachell <wachell@gmail.com>
2018-05-26 22:30:22 -0700

    stop for the night

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,36 +1,16 @@
-import React from 'react';
-import Carousel from 'nuka-carousel';
-import './Carousel.scss';
-import Images from './Images.jsx'
+import React from "react";
+import Carousel from "nuka-carousel";
+import "./Carousel.scss";
+import Images from "./Images.jsx";
 
 export default class Nuka extends React.Component {
-  afterSlide (currentSlide) {
-    const list = window.document.querySelector('.slider-list');
-    const nextSlide = list.childNodes[currentSlide];
-    list.style.height = nextSlide.offsetHeight + 'px';
-}
-
-  
   render() {
     return (
-   
-      <Carousel 
-                afterSlide={()=> this.afterSlide()}
-                width="65%"
-                className="carousel"             
-                cellAlign="center"
-                
-                slideWidth={0.8}
-                cellSpacing={300}
-                renderBottomCenterControls={()=> false}
-             
-                >
-        {
-          Images.first.map((img, key) => {
-            return (<img  className="vis" src={img} key={key}/>)
-          })
-        }
+      <Carousel width="55%" className="carousel">
+        {Images.first.map((img, key) => {
+          return <img src={img} key={key} />;
+        })}
       </Carousel>
     );
   }
-}        
+}

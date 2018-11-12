7a20b541dc361f60ab6f462cb8de5083bf824085
MIchael Wachell <wachell@gmail.com>
2018-11-12 12:44:09 -0800

    fixed contact page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Component } from 'react';
-import './Contact.scss';
-import axios from 'axios';
-import Github from '../../../Assets/github.svg';
-import Linked from '../../../Assets/linked.png';
-
-
+import React from "react";
+import ReactDom from "react-dom";
+import { Component } from "react";
+import "./Contact.scss";
+import axios from "axios";
+import Github from "../../../Assets/github.svg";
+import Linked from "../../../Assets/linked.png";
 
 export default class Contact extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      email: '',
-      name: '',
-      message: '',
+      email: "",
+      name: "",
+      message: "",
       sent: false
-    }
-
+    };
   }
 
   async sendMe() {
     try {
-      const message = await axios.post('/contact', {
+      const message = await axios.post("/contact", {
         from: this.state.email,
         name: this.state.name,
         message: this.state.message
@@ -32,44 +28,82 @@ export default class Contact extends Component {
 
       console.log(message);
     } catch (err) {
-      console.log(err, 'here yo error')
+      console.log(err, "here yo error");
     }
-
   }
 
   render() {
-    return (<div id="contactBG">
+    return (
+      <div id="contactBG">
+        <div id="form-main">
+          <div id="form-div">
+            <form className="form" id="form1">
+              <p className="name">
+                <input
+                  name="name"
+                  type="text"
+                  className="feedback-input"
+                  placeholder="Name"
+                  id="name"
+                  onChange={e => this.setState({ name: e.target.value })}
+                />
+              </p>
 
-      <div id="form-main" >
-        <div id="form-div">
-          <form className="form" id="form1">
+              <p className="email">
+                <input
+                  name="email"
+                  type="text"
+                  className="feedback-input"
+                  id="email"
+                  placeholder="Email"
+                  onChange={e => this.setState({ email: e.target.value })}
+                />
+              </p>
 
-            <p className="name">
-              <input name="name" type="text" className="feedback-input" placeholder="Name" id="name" onChange={(e) => this.setState({ name: e.target.value })} />
-            </p>
+              <p className="theText">
+                <textarea
+                  name="text"
+                  type="text"
+                  className="feedback-input"
+                  id="comment"
+                  placeholder="Comment"
+                  onChange={e => this.setState({ message: e.target.value })}
+                />
+              </p>
 
-            <p className="email">
-              <input name="email" type="text" className="feedback-input" id="email" placeholder="Email" onChange={(e) => this.setState({ email: e.target.value })} />
-            </p>
-
-            <p className="theText">
-              <textarea name="text" type="text" className="feedback-input" id="comment" placeholder="Comment" onChange={(e) => this.setState({ message: e.target.value })}></textarea>
-            </p>
-
-            <div className="submit">
-              <input type="submit" value="SEND" id="button-blue" onClick={() => this.sendMe()} />
-              <div className="ease"></div>
-            </div>
-          </form>
-        </div>
-      
-        
-      </div>
-      <div className="find-me" width="50%">
-     <div className="max-me" width="50%" align="center"> <a  href="https://github.com/michaelwachell" target="_blank"> <img width="50%"  src={Github}/> </a> </div>
-     <div className="max-me" width="50%" align="center"> <a  href="https://www.linkedin.com/in/mwachell
-" target="_blank"> <img width="50%" src={Linked}/> </a> </div>
+              <div className="submit">
+                <input
+                  type="submit"
+                  value="SEND"
+                  id="button-blue"
+                  onClick={() => this.sendMe()}
+                />
+                <div className="ease" />
+              </div>
+            </form>
           </div>
-    </div>)
+        </div>
+        <div className="find-me" style={{ marginTop: 30 }}>
+          <div className="max-me" style={{marginRight: 80}} align="center">
+         
+            <a href="https://github.com/michaelwachell" target="_blank">
+          
+              <img width="100%" src={Github} />
+            </a>
+          </div>
+          <div className="max-me" align="center">
+        
+            <a
+              href="https://www.linkedin.com/in/mwachell
+"
+              target="_blank"
+            >
+            
+              <img width="100%" src={Linked} />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
---
+++ client/src/components/Media_newbuild.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Parallax } from "react-spring";
+import "./Media.scss";
+import Copy from "./Text.jsx";
+import Images from "./Images.jsx";
+import Dep from "../../../Assets/coding_things.png";
+import Spot from "../../../Assets/spotifight.png";
+import Media from "../../../Assets/media_things.png";
+import Arrow from "../../../Assets/down-arrow.png";
+import Nuka from "./Carousel.jsx";
+
+// optional render for iframe.
+// get all assets for visual display.
+// make gifs of videos
+
+const mediaFlex = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "space-evenly",
+  padding: "159px"
+};
+
+const colContLeft = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  padding: "5px"
+};
+
+const colContRight = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  padding: "5px",
+  overflow: "scroll"
+};
+
+const child = {
+  flex: "1 0 25%",
+  display: "inline-block",
+  margin: "5px",
+  height: "100px",
+  color: "grey",
+  minWidth: "200px"
+};
+
+const codeSnip = {
+  margin: "5px",
+  minHeight: "100px",
+  minWidth: "500px",
+  color: "grey",
+  minWidth: "200px",
+  backgroundColor: "red"
+};
+
+const bigPic = {
+  margin: "5px",
+  height: "100px",
+  color: "white",
+  backgroundColor: "blue",
+  minWidth: "200px"
+};
+
+export default class Intro extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      word: "is da rocking word"
+    };
+  }
+
+  render() {
+    let rows = [];
+    let col = [];
+    for (let i = 0; i < 10; i++) {
+      rows.push(<div style={child}>YOU BORK YOU LORK YOU ROCK YOU ROLL</div>);
+    }
+
+    col.push(<div style={bigPic}>HERE IS THE BIG PICTURE</div>);
+    col.push(<div style={codeSnip}>HERE IS THE CODE SNIPPET</div>);
+
+    return (
+      <div style={mediaFlex}>
+        <div style={colContLeft}>{col}</div>
+
+        <div style={colContRight} className="no-scroll">{rows}</div>
+      </div>
+    );
+  }
+}
